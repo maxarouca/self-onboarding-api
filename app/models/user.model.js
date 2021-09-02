e06de38a3bf9ef00a4116484b117907c6a641d95
Maximiler Arouca <maximiler@gmail.com>
2021-09-02 15:42:55 -0300

    Fix auth model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,7 +1,7 @@
 module.exports = (mongoose) => {
   const schema = mongoose.Schema(
     {
-      name: { type: String, required: true, unique: true },
+      name: { type: String, required: true },
       email: { type: String, required: true, unique: true },
       password: { type: String, required: true },
       country: { type: String, required: true },
