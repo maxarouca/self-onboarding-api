af8248433f901f9088c2346603759ddec051b5a5
Maximiler Arouca <maximiler@gmail.com>
2021-09-02 18:08:30 -0300

    Fix register controller

--- app/routes/register.routes.js
+++ app/routes/register.routes.js
@@ -50,7 +50,7 @@ module.exports = (app) => {
 
   router.post("/upload", upload.single("file"), async (req, res) => {
     if (req.file === undefined) return res.send("you must select a file.");
-    const imgUrl = `${req.serverUrl}/file/${req.file.filename}`;
+    const imgUrl = `${req.serverUrl}/api/file/${req.file.filename}`;
     return res.send(imgUrl);
   });
 
